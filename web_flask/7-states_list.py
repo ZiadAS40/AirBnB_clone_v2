9d9378b34655511d5503cb94344abce1caaf23fd
ZiadAS40 <zead4036@gmail.com>
2024-08-17 20:00:03 +0300

    task number 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""state routing"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+objs = storage.all("State")
+state_objs = {}
+for k, v in objs.items():
+    temp = k.split('.')
+    state_objs.update({v.name: temp[1]})
+
+
+@app.teardown_appcontext
+def close(exception=None):
+    """tear down after session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """return html consit of state list"""
+    return render_template('7-states_list.html',
+                           states=state_objs)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
